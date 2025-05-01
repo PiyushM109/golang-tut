60826ba48ef33635e8e2f1a682668725fa6aba56
Piyush More <piyushmore109@gmail.com>
2025-05-01 23:54:47 +0530

    first commit

---
+++ 02Variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Piyush More"
+	fmt.Println(username)
+	fmt.Printf("variable is of type :  %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("variable is of type :  %T \n", isLoggedIn)
+
+	var anotherVaraible int
+	fmt.Println(anotherVaraible)
+	fmt.Printf("variable is of type :  %T \n", anotherVaraible)
+
+	var tempname string
+	fmt.Println(tempname)
+	fmt.Printf("variable is of type :  %T \n", tempname)
+}
